488f2c7821033a3a0350f90b777b0a0c3a0a411a
duciobanu <duciobanu@griddynamics.com>
2024-12-26 08:58:09 +0200

    Implement solution

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import userRoutes from "./routes/userRoutes.js";
+import exerciseRoutes from "./routes/exerciseRoutes.js";
+import dotenv from "dotenv";
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const HOST = "localhost";
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.use(userRoutes);
+app.use(exerciseRoutes);
 
-
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running at http://${HOST}:${PORT}`);
+});
